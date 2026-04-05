ec549be9d9db549c263e2c04a5f4d87d82694246
mavix21 <marcevizcarra2109@hotmail.com>
2026-04-05 12:00:59 -0500

    feat: add navigation hook with vim-style keybindings

---
+++ packages/lantern/src/hooks/use-navigation.ts
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { useApp, useInput } from 'ink';
+
+type UseNavigationProps = {
+	setCurrentSlide: React.Dispatch<React.SetStateAction<number>>;
+	totalSlides: number;
+};
+
+export const useNavigation = ({
+	setCurrentSlide,
+	totalSlides,
+}: UseNavigationProps): void => {
+	const lastKeyRef = React.useRef('');
+	const timeoutRef = React.useRef<NodeJS.Timeout | null>(null);
+
+	const { exit } = useApp();
+
+	useInput((input, key) => {
+		// Handle input here
+		if (input === 'q') {
+			exit();
+		}
+		if (key.rightArrow || input === 'l') {
+			setCurrentSlide((slide) => Math.min(slide + 1, totalSlides - 1));
+		}
+		if (key.leftArrow || input === 'h') {
+			setCurrentSlide((slide) => Math.max(slide - 1, 0));
+		}
+		if (input === 'G') {
+			setCurrentSlide(totalSlides - 1);
+		}
+		if (input === 'g') {
+			if (timeoutRef.current) {
+				clearTimeout(timeoutRef.current);
+			}
+
+			if (lastKeyRef.current === 'g') {
+				setCurrentSlide(0);
+				lastKeyRef.current = '';
+				return;
+			}
+
+			lastKeyRef.current = 'g';
+			timeoutRef.current = setTimeout(() => {
+				lastKeyRef.current = '';
+			}, 500);
+			return;
+		}
+
+		lastKeyRef.current = '';
+	});
+};
